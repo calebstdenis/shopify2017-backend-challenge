8101712026f2a468113b201becfc4c4595557d0a
calebstdenis <caleb.stdenis@outlook.com>
2017-05-04 17:04:37 -0400

    nailed down basic json parsing + test case

--- app/shopify-api-handler.js
+++ app/shopify-api-handler.js
@@ -1,26 +1,32 @@
 let http = require('http');
 let R = require('ramda');
+let Promise = require('es6-promise').Promise;
 
 module.exports = {
 parsePaginatedJSON: function(endpoint) {
     // R.range(1, getLastPage(endpoint))
-    // .map(parseApiPage(endpoint))
+    // .map(getJSON(endpoint))
     // .all();
 },
 getLastPage: function(endpoint) { },
 
-parseApiPage: R.curry(function(endpoint, pageNum) {
-    // let parsedData;
-    // http.get(`${endpoint}?page=${pageNum}`, (res) => {
-    //     let rawData;
-    //     res.on('data', (chunk) => rawData += chunk);
-    //     res.on('end', () => {
-    //         try {
-    //             parsedData = JSON.parse(rawData);
-    //         } catch(e) {
-    //             console.error(e.message);
-    //         }
-    //     });
-    // });
+
+getJSONFromPage: R.curry(function(endpoint, pageNum) {
+    return new Promise(function(resolve, reject) {
+        let parsedData;
+        http.get(`${endpoint}?page=${pageNum}`, (res) => {
+            let rawData = "";
+            res.on('data', (chunk) => rawData += chunk);
+            res.on('end', () => {
+                try {
+                    let result = JSON.parse(rawData)
+                    resolve(result);
+                }
+                catch(e) {
+                    reject(e);
+                }
+            });
+        });
+    })
 }),
 } 
--- test/shopify-api-handler.spec.js
+++ test/shopify-api-handler.spec.js
@@ -1,10 +1,30 @@
-let http = require('http');
-let assert = require('assert');
+const http = require('http');
+const PassThrough = require('stream').PassThrough;
+const testData = require ('./test-data');
+const sut = require('../app/shopify-api-handler')
+
+const sinon = require('sinon');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+require('chai').should();
 
 describe("Shopify Paginated API Handler", () => {
-    describe("parseApiPage", () => {
-        it("should make a GET call to the given endpoint and return a promise of the parsed JSON data", () => {
-            
+    let httpGetStub;
+
+    beforeEach(() => {
+        httpGetStub = sinon.stub(http, "get");
+    });
+    
+    describe("getJSON", () => {
+        it("should make a GET call to the given endpoint and return a promise of the parsed JSON data", (done) => {
+            const response = new PassThrough();
+            httpGetStub.callsArgWith(1, response);
+            let test = JSON.stringify(testData)
+            response.write(test);
+            response.end();
+
+            sut.getJSONFromPage("fakeEndpoint")(1).should.eventually.deep.equal(testData).notify(done);
         });
     })
 });
