cd359a73a8d31de613eb0a52c4b6e515321816c3
calebstdenis <caleb.stdenis@outlook.com>
2017-05-03 23:32:11 -0400

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "sourceType": "module",
+    },
+    "rules": {
+        "linebreak-style": ["error", "windows"],
+        "no-unused-vars": ["warn"]
+    }
+};
