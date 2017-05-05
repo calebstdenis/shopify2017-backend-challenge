f3352f44e573ef66357a35b65f885ef64a7d53b7
calebstdenis <caleb.stdenis@outlook.com>
2017-05-05 10:06:27 -0400

    final implementations for order api handler

--- app/order-api-handler.js
+++ app/order-api-handler.js
@@ -4,10 +4,20 @@ let Promise = require('es6-promise').Promise;
 
 module.exports = {
 parsePaginatedOrders: function(endpoint) {
-    // R.range(1, getLastPage(endpoint))
-    // .map(getJSON(endpoint))
-    // .all();
+    let promisedData = this.getLastPage(endpoint).then(lastPage => {
+        R.range(1, lastPage)
+        .map(this.getJSONFromPage(endpoint));
+    });
+    return Promise.all(promisedData).then(this.consolidatePagedData);
 },
+
+consolidatePagedOrders: function(pages) {
+    let allOrders = pages.map(page => page.orders)
+    let consolidatedData = Object.assign({}, pages[0], { orders: [].concat(allOrders) });
+    delete consolidatedData.pagination;
+    return consolidatedData;
+},
+
 getLastPage: function(endpoint) {
     return this.getJSONFromPage(endpoint)(1000000000).then(result => result.pagination.total)
 },
