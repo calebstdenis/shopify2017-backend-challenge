2bfb82cdd37b597184de535035093f534307a298
calebstdenis <caleb.stdenis@outlook.com>
2017-05-05 08:57:29 -0400

    finish BDD for api handler

--- test/order-api-handler.spec.js
+++ test/order-api-handler.spec.js
@@ -13,18 +13,22 @@ chai.use(sinonChai);
 require('chai').should();
 
 describe("Shopify Paginated API Handler", () => {
-    let httpGetStub;
+    let httpGetStub = sinon.stub(http, "get");
 
-    beforeEach(() => {
-        httpGetStub = sinon.stub(http, "get");
+    after(() => {
+        httpGetStub.reset();
     });
     
     describe("getJSON", () => {
+        let result;
+
         before(() => {
-            const response = new PassThrough();
-            httpGetStub.callsArgWith(1, response);
-            response.write(JSON.stringify(testData));
-            response.end();
+            httpResponse = new PassThrough();
+            httpGetStub.callsArgWith(1, httpResponse);
+            httpResponse.write(JSON.stringify(testData));
+            httpResponse.end();
+
+            result = sut.getJSONFromPage("http://fakeapi.com/orders.json")(1)
         });
 
         it("should make a GET request to the given API page", () => {
@@ -32,30 +36,75 @@ describe("Shopify Paginated API Handler", () => {
         });
 
         it("should return a promise of the parsed JSON data", (done) => {
-             sut.getJSONFromPage("http://fakeapi.com/orders.json")(1).should.eventually.deep.equal(testData).notify(done);
+            result.should.eventually.deep.equal(testData).notify(done);
         });
     });
 
     describe("getLastPage", () => {
-        it("should make a GET request to an arbitrary non-valid page to get just the pagination data", () => {
+        let result;
 
+        before(() => {
+            const responseData = Object.assign({}, testData); 
+            responseData.orders = [];
+            responseData.pagination.total = 10;
+            
+            httpResponse = new PassThrough();
+            httpGetStub.callsArgWith(1, httpResponse);
+            httpResponse.write(JSON.stringify(responseData));
+            httpResponse.end();
+
+            result = sut.getLastPage("http://fakeapi.com/orders.json")
         });
-        it("should return the number of pages based on the response pagination data", () => {
+        
+        it("should make a GET request to an arbitrary non-valid page (#1,000,000,000) to get just the pagination data", () => {
+            httpGetStub.should.have.been.calledWith("http://fakeapi.com/orders.json?page=1000000000");
+        });
 
+        it("should return a promise of the number of pages based on the response pagination data", (done) => {
+            result.should.eventually.equal(10).notify(done);
         });
     });
 
     describe("parsePaginatedJSON", () => {
-        it("should consolidate paged JSON into a single array of orders", () => {
+        let result;
 
+        const fakeOrdersPage1 = [{id: "fakeorder1"}];
+        const fakeOrdersPage2 = [{id: "fakeorder2"}];
+
+        before(() => {
+            const dataPage1 = Object.assign({}, testData);
+            const dataPage2 = Object.assign({}, testData);
+            const dataPageBillion = Object.assign({}, testData);
+            dataPage1.orders = fakeOrdersPage1;
+            dataPage2.orders = fakeOrdersPage2;
+            dataPageBillion.pagination.total = 2;
+
+            const responsePage1 = new PassThrough();
+            const responsePage2 = new PassThrough();
+            const responsePageBillion = new PassThrough();
+
+            httpGetStub.withArgs(sinon.match.string, 1).callsArgWith(1, responsePage1);
+            httpGetStub.withArgs(sinon.match.string, 2).callsArgWith(1, responsePage2);
+            httpGetStub.withArgs(sinon.match.string, 1000000000).callsArgWith(1, responsePageBillion);
+
+            responsePage1.write(JSON.stringify(dataPage1));
+            responsePage2.write(JSON.stringify(dataPage2));
+            responsePageBillion.write(JSON.stringify(dataPageBillion));
+            
+            result = sut.parsePaginatedOrders("http://fakeapi.com/orders.json");
+        })
+
+        it("should consolidate paged JSON into a single array of orders", () => {
+            let expectedOrders = fakeOrdersPage1.concat(fakeOrdersPage2);
+            result.should.have.members(expectedOrders);
         });
         
         it("should strip pagination data", () => {
-
+            result.should.not.have.property("pagination");
         });
 
-        it("should considate shared data among pages (shared cookies)", () => {
-
+        it("should also return the data shared among pages (e.g. available cookies)", () => {
+            result.should.have.property("available_cookies", testData["available_cookies"]);
         });
     });
 });
